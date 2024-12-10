53731a87a00b43c38ac9e9125c8853325ea284a3
moadsnow <elbaz.ouad1249@gmail.com>
2024-12-10 17:44:35 +0100

    InjectingPropreties

--- MyApp/src/main/java/com/java/springboot/demo/MyApp/rest/FunRestController.java
+++ MyApp/src/main/java/com/java/springboot/demo/MyApp/rest/FunRestController.java
@@ -1,10 +1,32 @@
 package com.java.springboot.demo.MyApp.rest;
 
+<<<<<<< HEAD
+=======
+import org.springframework.beans.factory.annotation.Value;
+>>>>>>> 44ec937 (InjectingPropreties)
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+<<<<<<< HEAD
+=======
+
+    // Inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private  String coachName;
+
+    @Value("${team.name}")
+    private  String teamhName;
+
+    // Expose new endpoint for "teaminfo"
+
+    @GetMapping("/teaminfo")
+    public String teaminfo() {
+        return "Coach: "+coachName + ", Team name: " + teamhName;
+    }
+
+>>>>>>> 44ec937 (InjectingPropreties)
     // expose "/" that return "Hello World"
 
     @GetMapping("/") //Hundl HTTP GET Requests
